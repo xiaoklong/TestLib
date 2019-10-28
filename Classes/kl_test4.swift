d4040a2e1a75a8367756e7ab1da480967c9681a8
肖康龙 <>
2019-10-28 11:14:27 +0800

    添加多一个文件

---
+++ Classes/kl_test4.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+enum KLTestEnum {
+    case one,two
+}
+
+class KLTestClass {
+    init() {
+        
+    }
+    func textPrint() {
+        print("testPrint")
+        print("testPrint0.0.3")
+    }
+}
